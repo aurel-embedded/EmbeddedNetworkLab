d36938740b9bd71ce6908896c88fa72659b7e88a
Aurelien <apajadon@hotmail.com>
2026-02-18 19:51:13 +0100

    feat(gui): gui ok

--- EmbeddedNetworkLab/UI/MainWindow.xaml.cs
+++ EmbeddedNetworkLab/UI/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EmbeddedNetworkLab
+namespace EmbeddedNetworkLab.UI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
