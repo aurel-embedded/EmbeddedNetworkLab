492119525a45dcbcba15caea3af7289806be8f0a
Aurelien <apajadon@hotmail.com>
2026-03-04 17:25:06 +0100

    feat(testingModule): adding liveCharts2 ==> working

--- EmbeddedNetworkLab/UI/Modules/TestingModuleUi/TestingModuleUi.xaml.cs
+++ EmbeddedNetworkLab/UI/Modules/TestingModuleUi/TestingModuleUi.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
--- EmbeddedNetworkLab/UI/Modules/TestingModuleUi/TestingModuleUiViewModel.cs
+++ EmbeddedNetworkLab/UI/Modules/TestingModuleUi/TestingModuleUiViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
 
 namespace EmbeddedNetworkLab.UI.Modules.TestingModuleUi
 {
@@ -10,6 +12,16 @@ namespace EmbeddedNetworkLab.UI.Modules.TestingModuleUi
 	{
 
 		public override string Name => "Testing Module";
+
+		// Exposed series for MVVM binding to the CartesianChart
+		public ISeries[] Series { get; } = new ISeries[]
+		{
+			new LineSeries<double>
+			{
+				Values = new double[] { 3, 5, 2, 8, 6 },
+				Fill = null
+			}
+		};
 
 
 	}
