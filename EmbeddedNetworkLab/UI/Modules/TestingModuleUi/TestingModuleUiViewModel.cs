a522d80d1493218a67e9d614cabab500deb2e763
Aurelien <apajadon@hotmail.com>
2026-03-04 16:48:33 +0100

    feat(testingModule): adding new module for testing ui management

---
+++ EmbeddedNetworkLab/UI/Modules/TestingModuleUi/TestingModuleUiViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmbeddedNetworkLab.UI.Modules.TestingModuleUi
+{
+	partial class TestingModuleUiViewModel : ModuleViewModel
+	{
+
+		public override string Name => "Testing Module";
+
+
+	}
+}
