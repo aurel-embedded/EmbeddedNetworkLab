09a0ddac1d2fbda72098731508c521afd95b9ede
Aurelien <apajadon@hotmail.com>
2026-03-09 17:19:56 +0100

    update(gui)

--- EmbeddedNetworkLab/UI/BoolToBrushConverter.cs
+++ EmbeddedNetworkLab/UI/BoolToBrushConverter.cs
@@ -7,8 +7,8 @@ namespace EmbeddedNetworkLab.UI.Converters
 {
 	public class BoolToBrushConverter : IValueConverter
 	{
-		public Brush TrueBrush { get; set; } = Brushes.Green;
-		public Brush FalseBrush { get; set; } = new SolidColorBrush(Color.FromRgb(0x33, 0x33, 0x33));
+ 		public Brush TrueBrush { get; set; } = new SolidColorBrush(Color.FromRgb(0x2E, 0x7D, 0x32)); // fallback success
+ 		public Brush FalseBrush { get; set; } = new SolidColorBrush(Color.FromRgb(0xD3, 0x2F, 0x2F)); // fallback error
 
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
